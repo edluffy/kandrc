b2b1c625f7bfa4aff162de19729ce5053ad1e025
edluffy <edward@ncade.com>
2018-07-07 12:59:55 +0100

    directory changes

--- ex1-2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("line 1\nline 2"); // prints a newline
-}
--- ex1-7.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("%i\n", EOF); // prints -1
-	printf("%c\n", EOF); // unknown (not in char set)
-}
