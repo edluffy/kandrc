d0d90ad2d7d32c9cc720bf015ce1aba8ff8c1eb0
edluffy <edward@ncade.com>
2018-07-14 19:43:48 +0100

    finished limit finder

--- ch-2/ex2-1.c
+++ ch-2/ex2-1.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <limits.h>
 /* program to determine ranges of:
- * char, short, int and long (unsigned and signed)
- * float, double */
+ * char, short, int and long 
+ * (unsigned and signed) */
 int main(void)
 {	
 	printf("\nSigned:\n");
-	printf("%i", CHAR_MAX, CHAR_MIN);
-	printf("%i", INT_MAX, SHORT_MIN);
-	printf("%i", CHAR_MAX, CHAR_MIN);
-	printf("%i", CHAR_MAX, CHAR_MIN);
+	printf("CHAR: %i, %i\n", CHAR_MAX, CHAR_MIN);
+	printf("SHRT: %i, %i\n", SHRT_MAX, SHRT_MIN);
+	printf("INT: %i, %i\n", INT_MAX, INT_MIN);
+	printf("LONG: %i, %i\n", LONG_MAX, LONG_MIN);
 	
 	printf("\nUnsigned:\n");
-	printf("%i", CHAR_MAX, CHAR_MIN);
-	printf("%i", CHAR_MAX, CHAR_MIN);
-	printf("%i", CHAR_MAX, CHAR_MIN);
-	printf("%i", CHAR_MAX, CHAR_MIN);
+	printf("UCHAR: %u, %i\n", UCHAR_MAX, 0);
+	printf("USHRT: %u, %i\n", USHRT_MAX, 0);
+	printf("UINT: %u, %i\n", UINT_MAX, 0);
+	printf("ULONG: %u, %i\n", ULONG_MAX, 0);
+}
