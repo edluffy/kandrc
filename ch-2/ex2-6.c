09f964483c70dfe178bb89ea5537715b73a276a5
edluffy <edward@ncade.com>
2018-07-17 20:40:59 +0100

    add setbit function

---
+++ ch-2/ex2-6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#define LIM 999
+/* write a function setbits(x, p, n, y) */
+
+int setbits(int x, int p, int n, int y);
+int main(void)
+{
+	int bits;
+
+	printf("Enter a number: ");
+	scanf("%i", &bits);
+ 	
+	printf("Result after bit manipulation: %i", setbits(bits, 3, 2, 4));
+}
+/* returns x with the n bits that begin at 
+ * pos. p set to the rightmost n bits of y */
+int setbits(int x, int p, int n, int y)
+{
+	int mask;
+	// copy n bits right of y
+	mask = ~(077 << n);
+
+	return (((x >> y-n) & mask) << p) | x;
+}
+
