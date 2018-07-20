9c9b651ae767d4f3310c1db92e41ea1edd02661c
edluffy <edward@ncade.com>
2018-07-20 10:07:56 +0100

    fix itoa to handle largest neg. int

--- ch-3/ex3-4.c
+++ ch-3/ex3-4.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
 #define LIM 999
-// converts n into base b rep. in string s
-void itob(int n, char s[], int b);
+
+void itoa(int n, char s[]);
 int main(void)
 {
-	char string[LIM]; // clear string
-	for(int i = 0; i < LIM; i++)
-		string[i] = 0;
-	itob(25, string, 2);
+	int i, number;
+	char string[LIM];
+	printf("Enter a number: ");
+	scanf("%i", &number);
+
+	itoa(number, string);
 	printf("%s", string);
 }
-void itob(int n, char s[], int b)
+/* convert n to characters in s
+ * rewritten to handle largest neg. no. */
+void itoa( int n, char s[])
 {
+	int i, sign;
+	// n is offset by 1 to fit largest neg. no.
+	n = ((sign = n) < 0) ? -n-1 : n-1;
+	
+	i = 0;
+	s[i++] = n % 10 + '1'; // offset is returned to normal
+	while((n /= 10) > 0)
+		s[i++] = n % 10 + '0';
 
+	if(sign < 0) 
+		s[i++] = '-';
+
+	s[i] = '\0';
+	strrev(s);
 }
