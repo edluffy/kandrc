8167e83c48ba75e1f540a31854571cc17f233b81
edluffy <edward@ncade.com>
2018-09-04 10:17:42 +0100

    changes to sscanf method

--- ch-7/ex7-5.c
+++ ch-7/ex7-5.c
@@ -1,36 +1,72 @@
 #include <stdio.h>
-#define MAXLL 99
+#define MAXLL 99 // maximum line length
+
+int readl(char *s, int lim);
+void push(double v);
+double pop(void);
 
 int main(void)
 {
-	// read line of input
-	// scroll through operations
-	// (manipulate stack and print after each line)
-	
-	while(readline(line, MAXLL) > 0){
-		while((type = getop(line, op)) != '\0'){
-			switch(op)
+	char line[MAXLL], op;
+	double v;
+
+	int pos = 0;
+	while(readl(line, MAXLL) > 0){
+		while(sscanf(line + pos++, " %lf", &v) == 1)
+			printf("|%lf|\n", v);
+		while(sscanf(line + pos++, "%c", &op) == 1){
+			printf("|%c|\n", op);
+			/*switch(op)
 			{
-			
-			}
+				case '+':
+					push(pop() + pop());
+					break;
+				default:
+					printf("error: unknown op - %c", op);
+					break;
+			} */
 		}
+		printf("\t%lf\n", pop());
 	}
+
+	return 0;
 }
 
-/* collect line of input, return length */
-int readline(char *s, maxl)
+/* gets next line of input, returns length */
+int readl(char *s, int lim)
 {
-	int c, l;
-	for(l = 0; (c = getchar()) != EOF && c != '\n' && l < maxl; l++)
-	       *s++ = c;
-	*s = '\0';
+	int len, c;
+	
+	len = 0;
+	while((c = getchar()) != EOF && c != '\n' && len < lim-1)
+		s[len++] = c;
+	s[len] = '\0';
 
-	return l;
+	return len;
 }
 
-int pos = 0;
+/* push and pop - calc stack manipulation */
 
-/* collect next legal operation/operand, return it and its type */
-int getop(char s[], int o)
+#define MAXNS 99 // maximum number of items in stack
+double nstack[MAXNS];
+int sp = 0;
+
+void push(double v)
 {
+	if(sp < MAXNS)
+		nstack[sp++] = v;
+	else
+		printf("push error: stack full\n");
+}
+
+double pop(void)
+{
+	if(sp > 0)
+		return nstack[--sp];
+	else{
+		printf("pop error: stack empty\n");
+		return 0;
+	}
+	
+
 }
